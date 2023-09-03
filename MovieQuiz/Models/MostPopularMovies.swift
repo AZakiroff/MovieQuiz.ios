5e80aaecfebbef02a8f5bb57126393b78694fdea
AZakiroff <anton.zakirov@gmail.com>
2023-09-03 19:40:32 +0700

    Updates on the sprint 06

---
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -0,0 +1,8 @@
+//
+//  MostPopularMovies.swift
+//  MovieQuiz
+//
+//  Created by Антон Закиров on 01.09.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Антон Закиров on 01.09.2023.
+//
+
+import Foundation
